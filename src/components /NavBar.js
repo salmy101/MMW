e94157b923870fe181c3adacb646eae72fa1f657
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2023-02-01 14:56:49 -0700

    workng on navbar

---
+++ src/components /NavBar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import logo from "./images/logo.png"
+import { FaBars, FaTimes } from "react-icons/fa";
+
+import "./NavBar.css"
+
+
+
+const NavBar = () => {
+  return (
+    <div className="header">
+      <nav className="navbar">
+        <a href="/" className="logo">
+         <img src={logo} alt='logo'></img>
+        </a>
+        <div className="hamburger">
+          <FaBars />
+        </div>
+        <ul className='nav-menu'>
+          <li className="'nav-item">
+            <a href='/'>Home</a>
+          </li>
+          <li className="'nav-item">
+            <a href='/'>About</a>
+          </li>
+          <li className="'nav-item">
+            <a href='/'>Resources</a>
+          </li>
+          <li className="'nav-item">
+            <a href='/'>Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
