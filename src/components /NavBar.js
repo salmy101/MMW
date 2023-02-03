2c0bfc6d9606d5527555c1966883d545a26e2cb9
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2023-02-02 22:28:12 -0700

    workng on navbar

--- src/components /NavBar.js
+++ src/components /NavBar.js
@@ -11,22 +11,23 @@ const NavBar = () => {
     <div className="header">
       <nav className="navbar">
         <a href="/" className="logo">
-         <img src={logo} alt='logo'></img>
+         {/* <img src={logo} alt='logo'></img> */}
+         <h1> LOGO</h1>
         </a>
         <div className="hamburger">
           <FaBars />
         </div>
         <ul className='nav-menu'>
-          <li className="'nav-item">
+          <li className="nav-item">
             <a href='/'>Home</a>
           </li>
-          <li className="'nav-item">
+          <li className="nav-item">
             <a href='/'>About</a>
           </li>
-          <li className="'nav-item">
+          <li className="nav-item">
             <a href='/'>Resources</a>
           </li>
-          <li className="'nav-item">
+          <li className="nav-item">
             <a href='/'>Contact</a>
           </li>
         </ul>
