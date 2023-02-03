5cd1a8997c688c6c70fe8fac4f5c3376a5704a1f
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2023-02-03 16:01:56 -0700

    nav bar complete

--- src/components /NavBar.js
+++ src/components /NavBar.js
@@ -1,23 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import logo from "./images/logo.png"
 import { FaBars, FaTimes } from "react-icons/fa";
 
 import "./NavBar.css"
 
 
-
 const NavBar = () => {
+  const [click, setClick] = useState(false)
+
+  const handleClick = () => setClick(!click) //the opposite of what it is
   return (
     <div className="header">
       <nav className="navbar">
         <a href="/" className="logo">
-         {/* <img src={logo} alt='logo'></img> */}
          <h1> LOGO</h1>
         </a>
-        <div className="hamburger">
-          <FaBars />
+        <div className="hamburger" onClick={handleClick}>
+          {click ?( <FaTimes size={30}/> ): (<FaBars size={30}/>) }
+          
         </div>
-        <ul className='nav-menu'>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
           <li className="nav-item">
             <a href='/'>Home</a>
           </li>
