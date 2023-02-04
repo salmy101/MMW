1c11d5da3c2c83ef2e2addc389abd8f2b8149961
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2023-02-03 21:08:29 -0700

    working on hereo section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import NavBar from './components/NavBar';
+import Hero from './components/Hero';
+
 
 function App() {
   return (
     <div >
       <NavBar />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="content">
+        <p>Edmonton Alberta</p>
+        <p>LOREM IPSUM</p>
+        <p>Muslim Mental Wealth</p>
+        <p>LOREM IPSUM</p>
+        <button href="/" className="button">
+          ABOUT US
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
